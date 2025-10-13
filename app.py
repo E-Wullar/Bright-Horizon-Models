e0ac9c0eb05d025eb568b7c66bc942105b9414b0
E-Wullar <evelyn.wullar@cogniserve.com>
2025-10-13 10:48:50 +0000

    app file - the main entry

---
+++ app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.title("Welcome to the Bright Horizon Models Dashboard")
+
+# Simple introduction or guidance
+st.write("Use the left sidebar to navigate through different apps.")
